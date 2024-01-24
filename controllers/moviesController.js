7beab1ed4198927602a85f7bdb12225c8087f13c
junfengqiao <jfqemma@gmail.com>
2024-01-23 21:32:26 -0500

    added addperformer controller

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,4 +1,5 @@
 const Movie = require('../models/movie')
+const Performer = require('../models/performer')
 
 /*
 router.post('/', movieCtrl.create)
@@ -38,6 +39,50 @@ exports.show = async function index(req, res) {
 
 }
 
-exports.addPerformer = async function addPerformer(req, res) {
+/*
+Associates a performer with a movie. (This will add performer to the movies cast array and the movie to the performers credits array). A frontend using this api endpoint might look something like this with the movie being pre-selected.
+router.post('/:movieId/Performers/:performerId', movieCtrl.addPerformer)
 
+exports.addPerformer = async function addPerformer(req, res) {
+    try {
+        const foundPerformer = await Performer.findOne({ _id: req.params.performerId}) // FIND THE PERFORMER
+        if(!foundPerformer) throw new Error(`Cound not find the performer with id ${req.params.performerId}` )
+        const foundMovie = await Movie.findOne({ _id: req.params.movieId}) // FIND THE PERFORMER
+        if(!foundMovie ) throw new Error('Cound not find the movie' )
+        foundMovie.cast.push(foundPerformer._id)
+        foundPerformer.credits.push(foundMovie._id)
+        await foundMovie.save()
+        await foundPerformer.save()
+        res.status(200).json({
+            msg: `successfully associate performer with id ${req.params.performerId} with movie with id ${req.params.movieId}`,
+            movie: foundMovie, 
+            performer: foundPerformer
+
+        })
+
+    } catch (error) {
+        res.status(400).json({msg: error.message})
+    }
+}
+*/
+
+exports.addPerformer = async function addPerformer(req, res) {
+    try {
+        const foundPerformer = await Performer.findOne({ _id: req.params.performerId })
+        if(!foundPerformer) throw new Error(`Could not locate performer with id ${req.params.performerId}`)
+        const foundMovie = await Movie.findOne({ _id: req.params.movieId })
+        if(!foundMovie) throw new Error(`Could not locate movie with id ${req.params.movieId}`)
+        // many to many
+        foundMovie.cast.push(foundPerformer._id)
+        foundPerformer.credits.push(foundMovie._id)
+        await foundMovie.save()
+        await foundPerformer.save()
+        res.status(200).json({
+            msg: `Sucessfully associate performer with id ${req.params.performerId} with movie with id ${req.params.movieId} `,
+            movie: foundMovie,
+            performer: foundPerformer
+        })
+    } catch (error) {
+        res.status(400).json({ msg: error.message })
+    }
 }
